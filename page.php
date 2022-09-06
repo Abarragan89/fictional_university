deb37a22fd79d0c9692c522bf97c3eabbfc88dd1
Anthony Barragan <anthony.bar.89@gmail.com>
2022-09-05 22:08:12 -0700

    Initial Commit

---
+++ page.php
@@ -0,0 +1,60 @@
+<?php
+get_header();
+while (have_posts()) {
+    the_post(); 
+    pageBanner();
+    ?>
+
+<div class="container container--narrow page-section">
+    <?php
+    $parentId = wp_get_post_parent_id(get_the_ID());
+    if ($parentId) { ?>
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+            <a class="metabox__blog-home-link" href="<?php echo get_permalink(
+                $parentId
+            ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to
+                <?php echo get_the_title($parentId); ?>
+            </a> <span class="metabox__main">
+                <?php the_title(); ?>
+            </span>
+        </p>
+    </div>
+    <?php }
+    ?>
+
+    <?php
+    $testArray = get_pages([
+        'child_of' => get_the_ID(),
+    ]);
+    if ($parentId or $testArray) { ?>
+    <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink(
+            $parentId
+        ); ?>">
+                <?php echo get_the_title($parentId); ?>
+            </a></h2>
+        <ul class="min-list">
+            <?php
+            if ($parentId) {
+                $findChildrenOf = $parentId;
+            } else {
+                $findChildrenOf = get_the_ID();
+            }
+            wp_list_pages([
+                'title_li' => null,
+                'child_of' => $findChildrenOf,
+                'sort_column' => 'menu_order',
+            ]);
+            ?>
+        </ul>
+    </div>
+    <div class="generic-content">
+        <?php the_content(); ?>
+    </div>
+</div>
+<?php }
+    ?>
+<?php
+}
+get_footer(); ?>
